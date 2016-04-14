04b7fdf19f37ead88f5e53e32f6dee029ad12e44
Kris <weldon.kris@gmail.com>
2016-04-14 16:40:19 -0400

    removed select method to  make grouping work in order to bring names into charts instead of station id

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,18 @@
 class HomesController < ApplicationController
+  require 'net/http'
+  require 'json'
+
+  def indego_api_response
+    uri = URI("https://www.rideindego.com/stations/json/")
+    Net::HTTP.get(uri)
+    #parsed = JSON.parse(response)
+  end
+
+
+
   def show
     @trips = BikeTrip.all
     @station = Station.all
-    @mytrips = Station.joins(:bike_trips).select(:station_name, :start_time)
+    @mytrips = Station.joins(:bike_trips)
   end
 end
